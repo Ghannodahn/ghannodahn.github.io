8d3ffa17050227869862c47681593701910c58a2
Joe Allan Muharsky <joemu@highlandscastleholdings.com>
2025-04-20 10:48:17 -0700

    Basic React/TypeScript implementation.

---
+++ library/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Hello World</h1>
+        <p>Welcome to my library</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
